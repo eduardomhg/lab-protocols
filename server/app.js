dc329c0bccb0ebfded1284cc759368a421c463a1
eduardomhg <eduardomhg@gmail.com>
2015-09-20 23:07:51 +0200

    Added node ExpressJS dependency and starting file. Changed .json files to include actual names and versions.

---
+++ server/app.js
@@ -0,0 +1,39 @@
+var pjson = require('../package.json');
+
+console.log('Lab Protocols web app version %s', pjson.version);
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(express.static('app'));
+
+app.get('/', function (req, res) {
+    console.log('route / requested');
+    return res.redirect('/index.html');
+});
+
+console.log('Connecting to database at %s...', 'mongodb://localhost/test')
+mongoose.connect('mongodb://localhost/test', function (err) {
+    if (err) {
+        console.log('Connection error: %s', err);
+        process.exit(1);
+    } else {
+        console.log('Connected to database');
+
+        var server = app.listen(8000, function () {
+		    var host = server.address().address;
+		    var port = server.address().port;
+		    
+		    console.log('ExpressJS Server listening at http://%s:%s', host, port);
+		});
+    }
+});
+
+
+
+
+
+
+
