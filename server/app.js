1dc943cc39bc8c585f242d79394d13418c2c511d
eduardomhg <eduardomhg@gmail.com>
2015-09-26 16:24:56 +0200

    Changed port to read from env variable.

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost/test', function (err) {
         console.log('Connected to database');
     }
 
-    var server = app.listen(8081, function () {
+    var server = app.listen(process.env.PORT || 8000, function () {
         var host = server.address().address;
         var port = server.address().port;
     
